123fb7415a161ede41fa1fdb4040b2f1a68559d2
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-06 14:02:04 -0700

    fixed testing bugs

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
+
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
-const locationData = require('./data/geo.json');
 const { mungeLocation } = require('./utils.js');
+const locationData = require('./data/geo.json');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.json({ hello: 'homies' });
-});
 
 app.get('/location', (req, res) => {
-    console.log(locationData);
-    constmungedData = mungeLocation(locationData);
-    res.json(locationData);
+    const mungedData = mungeLocation(locationData);
+
+    res.json(mungedData);
+});
 
 app.get('/weather', (req, res) => {
     console.log('you hit the weather route');
     res.json([
         {
-            "forecast": "Partly cloudy until afternoon.",
-            "time": "Mon Jan 01 2001"
+            'forecast': 'Partly cloudy until afternoon.',
+            'time': 'Mon Jan 01 2001'
         },
         {
-            "forecast": "Mostly cloudy in the morning.",
-            "time": "Tue Jan 02 2001"
+            'forecast': 'Mostly cloudy in the morning.',
+            'time': 'Tue Jan 02 2001'
         },
     ]);
 });
+
 
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
